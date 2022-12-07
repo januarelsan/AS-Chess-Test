71a02dc1dc85c3b9168360b90db7844237b1816b
januarelsan <januarelsan@gmail.com>
2022-12-08 02:16:02 +0700

    Update GameController script to SelectPieceManager

--- Assets/_Main/Scripts/SelectPieceManager.cs
+++ Assets/_Main/Scripts/SelectPieceManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class SelectPieceManager : MonoBehaviour
 {
     private bool isSelectingPiece;
     private Piece selectedPiece;
